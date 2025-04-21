fcafb81283821602605d249fb1b90288c701a8fc
kimyoel0912 <yoel0912@naver.com>
2025-04-22 01:13:40 +0900

    테스트 업로드

--- test.cpp
+++ test.cpp
@@ -1,1 +1,17 @@
-ffgggg
+#include <stdio.h>
+int main(void)
+{
+	double cm;
+
+	printf("키를 입력하시오(cm): ");
+	scanf("%lf", &cm);
+
+	double totalInch = cm / 2.54;
+	int feet = totalInch / 12;
+	double inch = totalInch - (feet * 12);
+	
+	
+	printf("%.1f는 %d피트 %.2f인치 입니다. ", cm, feet, inch);
+
+	return 0;
+}
